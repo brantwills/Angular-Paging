b9ea3f983c0d7bdf4e6a81fe5b7d262f9101d05c
Brant Wills <brantwills@gmail.com>
2013-11-18 17:00:05 -0500

    Adjust Paging Logic

--- paging.js
+++ paging.js
@@ -9,41 +9,48 @@ var app = angular.module('myApp', []);
 * The paging directive allows you to specify custom 
 *
 * @element E
-* @
+* 
+* todo: figure out best commenting structure 
+*
 */
-app.directive('paging',function () {
-    
+app.directive('paging', function () {
+
     // Private Count Variable
     var pageCount = 0,
         dots = '...',
-        adjacent = 3,
+        adjacent = 2,
         selectedClass = 'selected';
-    
-    
+
+
     // Add Dots ie: 1 2 [...] 10 11 12 [...] 56 57
     function addDots(scope) {
         scope.List.push({
-        	value: dots
+            value: dots
         });
     }
-    
-    
+
+
     // Add Range of Numbers
     function addRange(start, finish, scope) {
-                        
-        for (var i = start; i <= finish; i++) {
-            
+
+        var i = 0;
+        for (i = start; i <= finish; i++) {
+
             var item = {
-             	value: i,
+                value: i,
                 title: 'Page ' + i,
-                class: scope.page == i ? selectedClass : ''
+                class: scope.page == i ? selectedClass : '',
+                click: function (page) {
+
+                    scope.page = page;
+                }
             }
 
             scope.List.push(item);
-          }
-    } 
+        }
+    }
 
-    
+
     // Add First Pages
     function addFirst(scope) {
         addRange(1, 2, scope);
@@ -54,80 +61,89 @@ app.directive('paging',function () {
     function addLast(scope) {
         addDots(scope);
         addRange(pageCount - 1, pageCount, scope);
-    }    
-    
-    
-    function build(scope){
-        
-        if(!scope.pagesize) return;
-        if(!scope.page) scope.page = 1;
-        scope.page = parseInt(scope.page);
-        
-        var start = 1,
-            _adjacent = adjacent * 2,
-            finish = Math.ceil(scope.total / scope.pagesize); 	
-        
-        scope.List = [];
-        pageCount = finish;
-        
+    }
 
-        if (pageCount < (5 + _adjacent)) {
-            addRange(start, finish, scope);
+
+    function build(scope) {
+
+        if (!scope.pagesize) {
+            // to block divide by 0
+            return;
+        }
+
+        if (!scope.page) {
+            // default page is 1
+            scope.page = 1;
+        }
+
+        // convert to int
+        scope.page = parseInt(scope.page);
+
+        // clear list
+        scope.List = [];
+
+
+        // variables
+        var start = 1,
+            size = adjacent * 2,
+            pageCount = Math.ceil(scope.total / scope.pagesize);
+
+
+
+        if (pageCount < (5 + size)) {
+            addRange(start, pageCount, scope);
         }
         else {
-            
-            if (scope.page < (1 + _adjacent)) {
-                finish = 2 + _adjacent;
+
+            if (scope.page <= (1 + size)) {
+                var start = 1;
+                var finish = 2 + size;
                 addRange(start, finish, scope);
                 addLast(scope);
             }
-            else if (pageCount - _adjacent > scope.page && scope.page > _adjacent) {
-                
-                        
-                start = scope.page - _adjacent;
-                finish = scope.page + _adjacent;
-                
+            else if (pageCount - size > scope.page && scope.page > size) {
+                var start = scope.page - adjacent;
+                var finish = scope.page + adjacent;
                 addFirst(scope);
                 addRange(start, finish, scope);
                 addLast(scope);
             }
             else {
-                start = finish - (2 + _adjacent);
+                var start = finish - (1 + size);
+                var finish = pageCount;
                 addFirst(scope);
                 addRange(start, finish, scope);
             }
         }
     }
-    
-    
-    
-    
-	return {
-    	restrict: 'EA',
+
+
+
+
+    return {
+        restrict: 'EA',
         scope: {
-        	page: '@',
+            page: '@',
             pagesize: '@',
-            total: '@',
-            click: '&'
+            total: '@'
         },
-        template: 
-        '<ul>' + 
-        	'<li title="{{Item.title}}" class="{{Item.class}}" ng-repeat="Item in List track by $index">{{Item.value}}' +
+        template:
+        '<ul>' +
+        	'<li title="{{Item.title}}" class="{{Item.class}}" ng-click="Item.click(Item.value)" ng-repeat="Item in List track by $index">{{Item.value}}' +
         '</ul>',
-        link : function(scope, element, attrs)
-        {
+        link: function (scope, element, attrs) {
             build(scope);
-            
-            scope.$watch('total', function(){
-             	build(scope);   
+
+            scope.$watch('total', function () {
+                build(scope);
             });
-            
-            scope.$watch('page', function(){
-               	build(scope); 
+
+            scope.$watch('page', function () {
+                build(scope);
             });
-            
-            scope.$watch('pagesize', function(){
-               	build(scope); 
+
+            scope.$watch('pagesize', function () {
+                build(scope);
             });
         }
     }
