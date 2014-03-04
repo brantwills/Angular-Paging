4c87bab12f60a8a45b969b468b9df8cac44cddce
Brant Wills <brantwills@gmail.com>
2014-03-04 10:52:14 -0500

    Added Sample Controller
    Added a sample controller to support paging clicks.  The controller has
    one function which just writes the parameters to the console log

--- app.js
+++ app.js
@@ -3,6 +3,27 @@
 * @name ng.module:myApp
 * 
 * @description
-* This module is just here for sample purposes
+* This module is here for sample purposes
 */
 var app = angular.module('myApp', []);
+
+
+
+/**
+* @ngDoc controller
+* @name ng.module:myApp
+* 
+* @description
+* This controller is here for sample purposes
+*/
+app.controller('sampleCtrl',['$scope', function($scope){
+
+	// A function to do some act on paging click
+	// In reality this could be calling a service which 
+	// returns the items of interest from the server
+	// based on the page parameter
+	$scope.DoCtrlPagingAct = function(text, page){
+		console.log(text, page);
+	};
+
+}]);
