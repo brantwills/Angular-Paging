d270a55a45d998ff885588b7356ba14ecefdbc93
brantwills <brantwills@gmail.com>
2015-03-01 16:30:38 -0500

    Updating to match master

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 * @description
 * This module is here for sample purposes
 */
-var app = angular.module('myApp', []);
+angular.module('myApp', ["brantwills.paging"]);
 
 
 
@@ -16,14 +16,14 @@ var app = angular.module('myApp', []);
 * @description
 * This controller is here for sample purposes
 */
-app.controller('sampleCtrl',['$scope', function($scope){
+angular.module('myApp').controller('sampleCtrl',['$scope', function($scope){
 
-	// A function to do some act on paging click
-	// In reality this could be calling a service which 
-	// returns the items of interest from the server
-	// based on the page parameter
-	$scope.DoCtrlPagingAct = function(text, page){
-		console.log(text, page);
-	};
+    // A function to do some act on paging click
+    // In reality this could be calling a service which 
+    // returns the items of interest from the server
+    // based on the page parameter
+    $scope.DoCtrlPagingAct = function(text, page){
+        console.log(text, page);
+    };
 
 }]);
