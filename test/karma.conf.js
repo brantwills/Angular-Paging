d7019986d9d838202c305e516d6151d30b47ecf9
Brant Wills <brantwills@gmail.com>
2015-08-27 18:53:07 -0400

    Update: Switching to karma-spec-reporter

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['spec'],
 
 
         // web server port
@@ -60,7 +60,7 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
-
+        
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
